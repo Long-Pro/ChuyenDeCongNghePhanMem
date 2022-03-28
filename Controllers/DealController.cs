6085353d98b86260531bdf8f3855bb659b91995c
Long-Pro <nvl1682@gmail.com>
2022-03-28 20:58:07 +0700

    init

---
+++ Controllers/DealController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChuyenDeCongNghePhanMem.Controllers
+{
+    public class DealController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StockController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChuyenDeCongNghePhanMem.Controllers
+{
+    public class StockController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
